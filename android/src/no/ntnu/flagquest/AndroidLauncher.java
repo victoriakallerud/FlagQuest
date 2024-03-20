68dc448b64684d3e2493ef0ee199facce45e811c
Hanna Lunne <hanlun0804@outlook.com>
2024-03-20 14:04:39 +0100

    Push project setup to main
    local.properties file, in the root level, needs to be edited to where you have your SDKs

---
+++ android/src/no/ntnu/flagquest/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.ntnu.flagquest;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.ntnu.flagquest.FlagQuest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlagQuest(), config);
+	}
+}
