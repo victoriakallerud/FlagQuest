7788880fd04068f6b12da3c58554998f59f63c01
ll071 <44781682+LeoLTM@users.noreply.github.com>
2024-03-26 11:06:19 +0100

    Added Swagger Documentation generator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('FlagQuest Backend')
+    .setDescription('The API for the FlagQuest guessing game')
+    .setVersion('1.2')
+    .addTag('User')
+    .addTag('Lobby')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
