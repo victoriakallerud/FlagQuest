fe2ee17861116ec0d2499fad62c42c936f45a601
ll071 <44781682+LeoLTM@users.noreply.github.com>
2024-03-26 13:06:39 +0100

    Initial commit

---
+++ backend/src/user/IUserService.ts
@@ -0,0 +1,12 @@
+import { User } from '../interfaces/user.interface';
+
+export interface IUserService {
+    createUser(user: User): User;
+    getUser(userId: string): User;
+    updateUser(userId: string, user: User): User;
+    deleteUser(userId: string): void;
+    removeFriend(userId: string, friendId: string): void;
+    sendFriendRequest(userId: string, friendId: string): void;
+    acceptFriendRequest(userId: string, inquirerId: string): void;
+    rejectFriendRequest(userId: string, inquirerId: string): void;
+}
