90428174c426d7643cedbd900bfda230ac5599ae
ll071 <44781682+LeoLTM@users.noreply.github.com>
2024-03-26 13:06:29 +0100

    Added base implementation without database logic

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,14 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import { IUserService } from './IUserService';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 import { UserService } from './user.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'models';
+import { RequestUserDTO } from './dto/requestUser.dto';
 
 @ApiTags('user')
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @Get()
-    getHello(): string {
-      return this.userService.getHello();
+    @ApiOperation({ summary: 'Create a new user' })
+    @Post()
+    createUser(@Body() userDto: RequestUserDTO): User {
+      return this.userService.createUser(userDto);
+    }
+
+    @ApiOperation({ summary: 'Get a user by id' })
+    @Get(':userId')
+    getUser(@Param('userId') userId: string): User {
+      return this.userService.getUser(userId);
+    }
+
+    @ApiOperation({ summary: 'Update a user by id' })
+    @Put(':userId')
+    updateUser(@Param('userId') userId: string, @Body() userDto: RequestUserDTO): User{
+      return this.userService.updateUser(userId, userDto);
+    }
+
+    @ApiOperation({ summary: 'Delete a user by id' })
+    @Delete(':userId')
+    deleteUser(@Param('userId') userId: string): void {
+      return this.userService.deleteUser(userId);
+    }
+
+    @ApiOperation({ summary: 'Delete a user in the friendlist by id' })
+    @Delete(':userId/friends/:friendId')
+    removeFriend(@Param('userId') userId: string, @Param('friendId') friendId : string): void {
+      return this.userService.removeFriend(userId, friendId);
+    }
+
+    @ApiOperation({ summary: 'Send a Friendrequest to a user by id' })
+    @Put(':userId/friends/:friendId')
+    sendFriendRequest(@Param('userId') userId: string, @Param('friendId') friendId : string): void {
+      return this.userService.sendFriendRequest(userId, friendId);
+    }
+
+    @ApiOperation({ summary: 'Accept a Friendrequest from a inquirer by id' })
+    @Put(':userId/friends/requests/:inquirerId')
+    acceptFriendRequest(@Param('userId') userId: string, @Param('inquirerId') inquirerId : string): void {
+      return this.userService.acceptFriendRequest(userId, inquirerId);
+    }
+
+    @ApiOperation({ summary: 'Reject a Friendrequest from a inquirer by id' })
+    @Delete(':userId/friends/requests/:inquirerId')
+    rejectFriendRequest(@Param('userId') userId: string, @Param('inquirerId') inquirerId : string): void {
+      return this.userService.rejectFriendRequest(userId, inquirerId);
     }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable, Logger } from '@nestjs/common';
+import { IUserService } from './IUserService';
+import { User } from '../interfaces/user.interface';
+import { v4 as uuidv4 } from 'uuid';
+import { RequestUserDTO } from './dto/requestUser.dto';
 
 @Injectable()
-export class UserService {
+export class UserService implements IUserService {
 
-    getHello(): string {
-        return 'Hello World from the UserService!';
+    private readonly logger = new Logger(UserService.name);
+
+    createUser(user: User): User {
+        user.id = uuidv4();
+        user.creationTime = new Date().toISOString();
+        user.friendUuidList = [];
+        user.highScores = [];
+        user.lastOnline = new Date().toISOString();
+        return user;
+    }
+
+    getUser(userId: string): User {
+        //TODO: Implement database query
+        let mockUser: User = {
+            id: userId,
+            userName: 'mockUser',
+            friendUuidList: [],
+            pendingFriendRequests: [],
+            highScores: [],
+            lastOnline: new Date().toISOString(),
+            creationTime: new Date().toISOString(),
+        }
+        return mockUser;
+    }
+    
+    updateUser(userId: string, userDto: RequestUserDTO): User {
+      // find user with id in database and update lastonline and user body params   
+      // user.lastOnline = new Date().toISOString();
+        // TODO: Implement database update
+        let mockUser: User = {
+            id: userId,
+            userName: userDto.userName,
+            nationality: userDto.nationality,
+            friendUuidList: [],
+            pendingFriendRequests: [],
+            highScores: [],
+            lastOnline: new Date().toISOString(),
+            creationTime: new Date().toISOString(),
+        }
+        return mockUser;
+    }
+
+    deleteUser(userId: string): void {
+        //TODO implement database update
+        throw new HttpException('Not implemented', 501);
+    }
+
+    removeFriend(userId: string, friendId: string): void {
+      //TODO Check if friend is in friend list
+        // Find user in database and update it
+        throw new HttpException('Not implemented', 501);
+        return;
+    }
+    sendFriendRequest(userId: string, friendId: string): void {
+      //TODO Check if friend is in friend list, if not add friendrequest in pending friendrequests
+        throw new HttpException('Not implemented', 501);
+        return;
+    }
+
+    acceptFriendRequest(userId: string, inquirerId: string): void {
+        // TODO Check if inquirer is in friend list
+        // Find user in database and update it and delete pendingfriendrequest
+        throw new HttpException('Not implemented', 501);
+        return;
+    }
+    rejectFriendRequest(userId: string, inquirerId: string): void {
+        // TODO check if inquirer is in friend list 
+        // update pendingfriendrequest of User in Database
+        throw new HttpException('Not implemented', 501);
+        return;
     }
 }
