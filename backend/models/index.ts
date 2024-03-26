b0ab7e5eec526a78b51d72e30b5b3fe0ba810f5a
upveo <upveo@student.kit.edu>
2024-03-26 10:54:25 +0100

    generated model classes

---
+++ backend/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnswerOption';
+export * from './Lobby';
+export * from './Nationality';
+export * from './Options';
+export * from './Question';
+export * from './Quiz';
+export * from './RequestLobby';
+export * from './RequestUser';
+export * from './Score';
+export * from './User';
