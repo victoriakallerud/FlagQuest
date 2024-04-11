bdbefe8ef0fe8a651887f6863de3a483d8746451
Hanna Lunne <hanlun0804@outlook.com>
2024-04-11 13:35:10 +0200

    Implement Pair in button array in main menu

--- client/core/src/com/flagquest/game/states/MainMenuState.kt
+++ client/core/src/com/flagquest/game/states/MainMenuState.kt
@@ -25,7 +25,7 @@ class MainMenuState(gsm: GameStateManager) : State(gsm) {
     private val trainingBtn = TextButton("TRAINING MODE", skin)
     private val highscoreBtn = TextButton("HIGHSCORE BOARD", skin)
     private val friendsBtn = TextButton("MANAGE FRIENDS", skin)
-    private val buttons = arrayOf(createBtn, joinBtn, trainingBtn, highscoreBtn, friendsBtn)
+    private val buttons = arrayOf(Pair(createBtn, "LobbyInitiationState"), Pair(joinBtn, "JoinGameState"), Pair(trainingBtn, "NOT IMPLEMENTED"), Pair(highscoreBtn, "HighscoreState"), Pair(friendsBtn, "ManageFriendsState"))
 
     init {
         Gdx.input.inputProcessor = stage
@@ -36,11 +36,11 @@ class MainMenuState(gsm: GameStateManager) : State(gsm) {
         stage.addActor(heading)
         titleFont.data.setScale(1.5f)
         for (button in buttons) {
-            button.setSize((screenWidth*80/100).toFloat(), buttonHeight.toFloat())
-            button.setPosition(screenWidth / 2 - button.width / 2, pos)
-            button.addListener(ButtonClickListener(gsm,button.text.toString()))
-            stage.addActor(button)
-            pos -= button.height + 30
+            button.first.setSize((screenWidth*80/100).toFloat(), buttonHeight.toFloat())
+            button.first.setPosition(screenWidth / 2 - button.first.width / 2, pos)
+            button.first.addListener(ButtonClickListener(gsm,button.second))
+            stage.addActor(button.first)
+            pos -= button.first.height + 30
         }
     }
 
