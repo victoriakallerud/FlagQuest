eecbf59fe59ba6335fa93c13a2a8bd07e69285f7
imeanvmiweg <ameldekok@live.nl>
2024-04-14 00:15:39 +0200

    Update OnlineGameState.kt
    Correct colors

--- client/core/src/com/flagquest/game/states/OnlineGameState.kt
+++ client/core/src/com/flagquest/game/states/OnlineGameState.kt
@@ -60,7 +60,7 @@ class OnlineGameState(gsm: GameStateManager, chosen: String? = null, correct: St
             }
 
             if (country == chosen && country != correct)
-                button.setColor(0.286F, 0.612F, 0.384F,1.0f)
+                button.setColor(.892f,.393f,.37f,1f)
             if (country == correct)
                 button.setColor(0.286F, 0.612F, 0.384F,1f)
 
