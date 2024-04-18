2ea3695f03d889c9607c7bfe6ec5317cc93baa85
victoriakallerud <victoriakallerud@gmail.com>
2024-04-18 13:59:06 +0200

    feat(#5): fixed loginBtn and regBtn, and cleaned up RegistrationState & LoginState code

--- client/core/src/com/flagquest/game/states/LoginState.kt
+++ client/core/src/com/flagquest/game/states/LoginState.kt
@@ -3,53 +3,91 @@ package com.flagquest.game.states
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.scenes.scene2d.InputEvent
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.badlogic.gdx.utils.viewport.ScreenViewport
+import com.flagquest.game.models.AuthHandler
 import com.flagquest.game.utils.UIManager.addBackButton
 import com.flagquest.game.utils.UIManager.addHeading
-import com.flagquest.game.utils.UIManager.addNavButton
 
-class LoginState(gsm: GameStateManager) : State(gsm) {
+class LoginState(gsm: GameStateManager, private val authHandler: AuthHandler) : State(gsm) {
     private val skin: Skin = Skin(Gdx.files.internal("skins/skin/flat-earth-ui.json"))
-    private val textFieldStyle: TextField.TextFieldStyle = skin.get(TextField.TextFieldStyle::class.java)
     private val titleFont: BitmapFont = skin.getFont("title")
     private var screenWidth = Gdx.graphics.width
     private val screenHeight = Gdx.graphics.height
     private val buttonHeight = screenHeight / 11
-    private var pos: Float = ((screenHeight / 2) + 50).toFloat()
     override val stage = Stage(ScreenViewport())
 
-    private val usernameField = TextField("", skin).apply{ messageText="  Username"}
+    private val emailField = TextField("", skin).apply{ messageText="  E-mail"}
     private val passwordField = TextField("", skin).apply{
-        messageText="  Password"
-        isPasswordMode=true
+        messageText = "  Password"
+        isPasswordMode = true
         setPasswordCharacter('*')
     }
-    val loginBtn = TextButton("LOGIN", skin) to lazy {MainMenuState(gsm)}
-    val buttonY = pos - (buttonHeight + 30) * 2
+
+    private val loginBtn = TextButton("LOGIN", skin) to lazy {loginUser(emailField.text, passwordField.text)}
+    private val inputFields = arrayOf(emailField, passwordField)
 
     init {
-        textFieldStyle.font.data.setScale(5f)
         titleFont.data.setScale(1.5f)
 
         addHeading(stage,"LOGIN", 2.8f)
 
-        usernameField.width = (screenWidth*80/100).toFloat()
-        usernameField.height = buttonHeight.toFloat()
-        usernameField.setPosition(screenWidth / 2 - usernameField.width / 2, pos)
-        stage.addActor(usernameField)
+        var posY = screenHeight / 2 + 50f
 
-        passwordField.width = (screenWidth*80/100).toFloat()
-        passwordField.height = buttonHeight.toFloat()
-        passwordField.setPosition(screenWidth / 2 - passwordField.width / 2, pos - buttonHeight - 30)
-        stage.addActor(passwordField)
+        for (input in inputFields) {
+            input.width = (screenWidth*80/100).toFloat()
+            input.height = buttonHeight.toFloat()
+            input.setPosition((screenWidth - input.width) / 2, posY)
+            stage.addActor(input)
+            posY -= (buttonHeight + 30)
+        }
 
-        addNavButton(stage,gsm, loginBtn,buttonY)
+
+        loginBtn.first.addListener(object : ClickListener() {
+            override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                loginUser(emailField.text, passwordField.text)
+            }
+        })
+        loginBtn.first.setSize((screenWidth*80/100).toFloat(), buttonHeight.toFloat())
+        loginBtn.first.setPosition(screenWidth / 2 - loginBtn.first.width / 2, posY)
+        stage.addActor(loginBtn.first)
+
         addBackButton(stage,gsm, backNavType)
     }
+
+
+    private fun loginUser(email: String, password: String) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Gdx.app.log("LoginState", "Email or password cannot be empty")
+            return
+        }
+        Gdx.app.log("LoginState", "Attempting to login with email: $email")
+        authHandler.signIn(email, password) { success, message ->
+            if (success) {
+                Gdx.app.log("LoginState", "Login successful")
+                updateUI(true)
+            } else {
+                Gdx.app.log("LoginState", "Login failed: $message")
+                updateUI(false)
+            }
+        }
+    }
+
+    private fun updateUI(isLoggedIn: Boolean) {
+        Gdx.app.postRunnable {
+            if (isLoggedIn) {
+                gsm.set(MainMenuState(gsm))
+            } else {
+                // TODO: Handle failed login. Show error message
+            }
+        }
+    }
+
 
     override fun handleInput() {
         screenWidth = screenWidth // Just placeholder code to make the code compile
