8097897ba92dbda6afd4d1b466cd8ff298628dff
Hanna Lunne <92915559+hanlun0804@users.noreply.github.com>
2024-04-18 16:40:11 +0200

    Update OfflineGameController.kt

--- client/core/src/com/flagquest/game/controllers/OfflineGameController.kt
+++ client/core/src/com/flagquest/game/controllers/OfflineGameController.kt
@@ -6,10 +6,6 @@ import com.flagquest.game.models.Quiz
 import com.flagquest.game.navigation.TrainingQuestionRedirectionListener
 
 class OfflineGameController(private val model: LocalApiModel) {
-//    fun handleCreateOfflineGame(numberOfQuestions: Int, region: String): Quiz {
-//        return model.generateQuiz(numberOfQuestions, region)
-//    }
-
     var redirectionListener: TrainingQuestionRedirectionListener? = null
 
     fun getSingleQuestion(region: String): Question {
