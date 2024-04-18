240940e26c71e5b888e6bb99ae90b908dda4d307
Hanna Lunne <hanlun0804@outlook.com>
2024-04-18 17:33:10 +0200

    Restructure LoginMenuState to follow MVC pattern

--- client/core/src/com/flagquest/game/states/LoginMenuState.kt
+++ client/core/src/com/flagquest/game/states/LoginMenuState.kt
@@ -2,36 +2,32 @@ package com.flagquest.game.states
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 import com.flagquest.game.models.AuthHandler
-import com.flagquest.game.utils.UIManager.addHeading
-import com.flagquest.game.utils.UIManager.addNavButtonArray
+import com.flagquest.game.utils.ButtonClickListener
+import com.flagquest.game.views.LoginMenuView
 
 class LoginMenuState(gsm: GameStateManager, private val authHandler: AuthHandler) : State(gsm) {
-    private val skin: Skin = Skin(Gdx.files.internal("skins/skin/flat-earth-ui.json"))
-    val titleFont: BitmapFont = skin.getFont("title")
-    var screenWidth = Gdx.graphics.width
-    val screenHeight = Gdx.graphics.height
-    var pos: Float = ((screenHeight / 2) + 50).toFloat()
     override val stage = Stage(ScreenViewport())
-
-    val loginBtn = TextButton("LOGIN", skin)
-    val registerBtn = TextButton("REGISTER", skin)
-    val buttons = arrayOf(Pair(loginBtn, lazy { LoginState(gsm, authHandler) }),Pair(registerBtn, lazy { RegistrationState(gsm, authHandler) }))
+    private val view = LoginMenuView(gsm, stage, authHandler)
 
     init {
-        titleFont.data.setScale(1.5f)
-        addHeading(stage,"FLAGQUEST",3.5f)
-        addNavButtonArray(stage,gsm,buttons,pos)
+        Gdx.input.inputProcessor = stage
+        setUpButtonListeners()
     }
 
+    private fun setUpButtonListeners() {
+        view.buttons[0].addListener(ButtonClickListener(gsm, lazy { LoginState(gsm, authHandler) }))
+        view.buttons[1].addListener(
+            ButtonClickListener(
+                gsm,
+                lazy { RegistrationState(gsm, authHandler) })
+        )
+    }
 
     override fun handleInput() {
-        screenWidth = screenWidth // Just placeholder code to make the code compile
+        // TODO: Implement handleInput
     }
     override fun update(dt: Float) {
         handleInput()
@@ -42,4 +38,5 @@ class LoginMenuState(gsm: GameStateManager, private val authHandler: AuthHandler
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         stage.draw()
     }
+
 }
