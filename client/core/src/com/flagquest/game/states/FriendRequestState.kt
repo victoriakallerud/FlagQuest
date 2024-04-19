4e346d2ea0400b8fa509b065514f58c0612ad788
Hanna Lunne <hanlun0804@outlook.com>
2024-04-19 10:00:52 +0200

    Cleanup

--- client/core/src/com/flagquest/game/states/FriendRequestState.kt
+++ client/core/src/com/flagquest/game/states/FriendRequestState.kt
@@ -2,27 +2,13 @@ package com.flagquest.game.states
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
-import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
-import com.badlogic.gdx.scenes.scene2d.ui.Skin
-import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
 import com.badlogic.gdx.utils.viewport.ScreenViewport
-import com.flagquest.game.utils.FriendRequestListener
-import com.flagquest.game.utils.UIManager.addBackButton
-import com.flagquest.game.utils.UIManager.addHeading
 import com.flagquest.game.views.FriendRequestView
-import com.flagquest.game.views.LobbyInitiationView
 
 class FriendRequestState(gsm: GameStateManager) : State(gsm) {
     override val stage = Stage(ScreenViewport())
-    private val view = FriendRequestView(gsm, stage/*, this*/)
-
+    private val view = FriendRequestView(gsm, stage)
 
     init {
         Gdx.input.inputProcessor = stage
