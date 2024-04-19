d3b24a7283d4d7c1aa1d48a189c0a9b44378225b
ll071 <44781682+LeoLTM@users.noreply.github.com>
2024-04-19 18:40:05 +0200

    Moved socket logic to GameApiModel

--- client/core/src/com/flagquest/game/controllers/GameLobbyController.kt
+++ client/core/src/com/flagquest/game/controllers/GameLobbyController.kt
@@ -1,15 +1,25 @@
 package com.flagquest.game.controllers
 
+import com.badlogic.gdx.Gdx
 import com.flagquest.game.models.LobbyApiModel
 import com.flagquest.game.models.UserApiModel
+import com.flagquest.game.navigation.GameRedirectionListener
+import com.flagquest.game.navigation.OnlineGameRedirectionListener
+import com.flagquest.game.utils.DataManager
 import com.flagquest.game.utils.SocketHandler
 import io.socket.client.Socket
 import org.json.JSONArray
 import org.json.JSONObject
 
 class GameLobbyController(private val userModel: UserApiModel, private val lobbyModel: LobbyApiModel) {
-    //var redirectionListener: LobbyRedirectionListener? = null
-    private lateinit var mSocket: Socket
+    var redirectionListener: OnlineGameRedirectionListener? = null
+
+    fun onGameStartButtonClicked(lobbyId: String) {
+        SocketHandler.emit("startGame", JSONObject().put("lobbyId", lobbyId).put("userId", DataManager.getData("userId")))
+        // Wait for 2 seconds
+        Thread.sleep(2000)
+        redirectionListener?.redirectToOnlineGameState()
+    }
 
     fun onLoadLobby(lobbyId: String): String? {
         val lobby = lobbyModel.getLobby(lobbyId)
@@ -22,47 +32,6 @@ class GameLobbyController(private val userModel: UserApiModel, private val lobby
         return jsonPlayer.getString("userName")
     }
 
-    fun connectToSocket(): Socket {
-        SocketHandler.setSocket()
-        SocketHandler.establishConnection()
-
-        mSocket = SocketHandler.getSocket()
-
-        mSocket.on(Socket.EVENT_CONNECT) {
-            println("Connected to server")
-        }
-        mSocket.on("updateLobby") { args ->
-            val message = args[0] as JSONObject
-            println("Lobby updated: ${message.getString("name")} - ${message.getString("state")} - ${message.getString("id")}")
-        }
-        mSocket.on("status") { args ->
-            val message = if (args[0] is String) JSONObject(args[0] as String) else args[0] as JSONObject
-            val status = message.getString("status")
-            val statusMessage = if (message.get("message") is JSONArray) {
-                val messageArray = message.getJSONArray("message")
-                messageArray.joinToString(", ")
-            } else {
-                message.getString("message")
-            }
-            if (status == "ERROR") {
-                println("Status [ERROR]: $statusMessage")
-                // joinLobby(socket)
-            } else {
-                println("Status [SUCCESS]: $statusMessage")
-                // startGame(socket)
-            }
-        }
-
-        return mSocket
-    }
-
-    fun joinLobby(socket: Socket, userId: String, lobbyId: String) {
-        val joinBody = JSONObject()
-        joinBody.put("userId", userId)
-        joinBody.put("lobbyId", lobbyId)
-        println("Joining lobby with object: ${joinBody.toString()}")
-        socket.emit("joinLobby", joinBody)
-    }
 
     fun getMaxPlayers(lobby: String): Int {
         val options = JSONObject(lobby).getJSONObject("options")
